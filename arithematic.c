e766da4665b787b530735f2980c175953adb6be4
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-09 16:00:59 +0530

    c pgm
    good

---
+++ arithematic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int n,a,d,s,ans=0,i;
+
+scanf("%d",&n);
+scanf("%d",&a);
+scanf("%d",&d);
+for(i=1;i<=n;i++)
+{
+s=a+(i-1)*d;
+ans=ans+s;
+}
+printf("%d",ans);
+return 0;
+}
